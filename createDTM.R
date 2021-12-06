843aab9d3f085500cc0210521f9a195cfd2e99c2
brownhr <brownhr314@gmail.com>
2021-12-06 18:49:36 -0500

    Create DTM Function

---
+++ createDTM.R
@@ -0,0 +1,16 @@
+create_dtm <- function(doc_vec, doc_names, ...) {
+    dtm <- CreateDtm(
+        doc_vec = doc_vec,
+        doc_names = doc_names,
+        ngram_window = c(1, 3),
+        stopword_vec = c(
+            twitter_stopwords,
+            stopwords::stopwords(language = "en"),
+            stopwords::stopwords(source = "smart")
+        ),
+        stem_lemma_function = function(x) {
+            SnowballC::wordStem(x, language = "porter")
+        }
+    )
+    return(dtm)
+}
